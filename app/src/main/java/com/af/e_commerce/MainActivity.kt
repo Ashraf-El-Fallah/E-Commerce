eb01faf719cd1647841c3c0e1e7f296d7998e8a0
Ashraf Abd El-Monem Mahmoud El-Fallah <elfallahashraf44@gmail.com>
2024-08-06 12:14:58 +0300

    add firebase dependencies

--- app/src/main/java/com/af/e_commerce/MainActivity.kt
+++ app/src/main/java/com/af/e_commerce/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AnticipateInterpolator
+import android.widget.TextView
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         initSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.text_crash).setOnClickListener {
+            throw RuntimeException("Test Crash")
+        }
     }
 
     private fun initSplashScreen() {
